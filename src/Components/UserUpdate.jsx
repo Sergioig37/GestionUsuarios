e97f41a55b6d30bc6a90bc6fc14b791aadbe8e97
Sergioig37 <sergioig37@educastur.es>
2024-03-10 20:07:42 +0100

    updaten

--- src/Components/UserUpdate.jsx
+++ src/Components/UserUpdate.jsx
@@ -12,11 +12,12 @@ import {Avatar} from "@mui/material";
 
 export const UserUpdate = () => {
 
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
+  const [nombre, setNombre] = useState('');
   const [apellido, setApellido] = useState('');
   const [avatar, setAvatar] = useState('');
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState('');
 
   
 
@@ -24,15 +25,20 @@ export const UserUpdate = () => {
     fetch(`https://www.melivecode.com/api/users/${id}`)
       .then((res) => res.json())
       .then((result) => {
+        setNombre(result.user.fname);
         setApellido(result.user.lname);
         setAvatar(result.user.avatar);
+        setUsername(result.user.username);
       });
   },[]);
 
   const updateUser = () => {
     var data = {
       id: id,
-      lname: apellido
+      fname: nombre,
+      lname: apellido,
+      username: username,
+      avatar: avatar,
     };
   
     fetch('https://www.melivecode.com/api/users/update', {
@@ -67,17 +73,47 @@ export const UserUpdate = () => {
     </Typography>
     <form sx={{ width: "100%", marginTop: "3" }} onSubmit={updateUser} action="PUT">
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            name="lastName"
-            variant="outlined"
-            required
-            label="Apellido"
-            value={apellido}
-            onChange={(e) => setApellido(e.target.value)}
-            autoFocus
-          />
-        </Grid>
+      <Grid item xs={12} sm={6}>
+            <TextField
+              name="firstName"
+              variant="outlined"
+              required
+              label="Nombre"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="lastName"
+              variant="outlined"
+              required
+              label="Apellido"
+              value={apellido}
+              onChange={(e) => setApellido(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="username"
+              variant="outlined"
+              required
+              label="Nombre de Usuario"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              name="avatar"
+              variant="outlined"
+              required
+              label="Avatar"
+              value={avatar}
+              onChange={(e) => setAvatar(e.target.value)}
+            />
+          </Grid>
       </Grid>
       <Button
         type="submit"
